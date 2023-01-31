15b907d7a363746711925889ec5cc1a7d2c871fe
1JeongYeon <102153402+1JeongYeon@users.noreply.github.com>
2023-01-31 22:24:13 +0900

    뭐가 잘 안됨 유도탄

--- Assets/Resources/Scripts/InGame/Bullet/Bullet.cs
+++ Assets/Resources/Scripts/InGame/Bullet/Bullet.cs
@@ -21,15 +21,9 @@ public abstract class Bullet : MonoBehaviour
     private bool shootAble = false;
     private float delayTime = 0f;
 
-    private GameObject playerTrans;
-    Rigidbody2D bulletRigidbody;
     private void Start()
     {
-        playerTrans = FindObjectOfType<Player>().gameObject;
-        bulletRigidbody = GetComponent<Rigidbody2D>();
-        bulletRigidbody.gravityScale = 0f;
-        // Z축 회전 무시
-        bulletRigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        delayTime = bulletData.delayTime;
     }
     public abstract void InitSetting(TMP_Text info);
 
@@ -42,17 +36,7 @@ public abstract class Bullet : MonoBehaviour
             var bullet = Instantiate(bulletData.bullet);
             bullet.transform.position = muzzle.position;
 
-            // 플레이어의 방향을 구함 (normalized : 방향 유지 길이가 1인 벡터 생성. 일정한 속도)
-            Vector3 dir = (playerTrans.transform.position - bullet.transform.position).normalized;
-
-            float vectorX = dir.x * bulletData.bulletSpeed;
-            float vectorY = dir.y * bulletData.bulletSpeed;
-
-            // 해당 방향으로 속도 세팅
-            bulletRigidbody.velocity = new Vector2(vectorX, vectorY);
-            // x축을 넘어가면 반전 시키기
-            bullet.GetComponent<SpriteRenderer>().flipX = (vectorX < 0);
-
+            
             var fireEffect = Instantiate(soundText);
             fireEffect.transform.position = muzzle.position + new Vector3(0, 3f, 0);
             fireEffect.text = bulletData.soundEffect;
--- Assets/Resources/Scripts/InGame/Bullet/BulletController.cs
+++ Assets/Resources/Scripts/InGame/Bullet/BulletController.cs
@@ -9,13 +9,24 @@ public class BulletController : MonoBehaviour
     // 마지막으로 쏘았던 총알이 현 총알과 다를 경우에 초기화 시켜주기 위해 기억해 주기 위한 변수
     private Bullet lastBullet;
 
+    private BulletData bulletData;
+
     public Transform muzzle;
     public TMP_Text effect;
     public TMP_Text info;
 
+    private Transform playerTrans;
+    private Transform bulletTrans;
+    private float dis;
+    private float delay;
+
     void Start()
     {
-
+        dis = Vector3.Distance(bulletTrans.position, playerTrans.position);
+        bulletData.bullet.transform.rotation = Quaternion.LookRotation(transform.position - playerTrans.position);
+        playerTrans = FindObjectOfType<Player>().transform;
+        bulletTrans = bulletData.bullet.transform;
+        delay = bulletData.delayTime;
         lastBullet = currentBullet;
         currentBullet.InitSetting(info);
     }
@@ -30,6 +41,7 @@ public class BulletController : MonoBehaviour
         }
 
         currentBullet.Shooting(muzzle, effect);
+        DiffusionMissileMoveOperation();
 
         // 이벤트성 총알이 다 소모되었을 경우 기본 총알로 변경해 시간이 끝날때 까지 쏘게 한다.
         if (currentBullet.bulletData.maxBullet == 0)
@@ -37,4 +49,29 @@ public class BulletController : MonoBehaviour
             currentBullet = GetComponent<DefaultBullet>();
         }
     }
+
+    private void DiffusionMissileMoveOperation()
+    {
+        delay += Time.deltaTime;
+        if (delay < 1f)
+        {
+            // 1초동안 천천히 앞으로 전진
+            bulletData.bulletSpeed = Time.deltaTime;
+            transform.Translate(bulletTrans.forward * bulletData.bulletSpeed, Space.Self);
+        }
+        else
+        {
+            // 1초 이후 타겟방향으로 lerp위치이동 합니다
+            bulletData.bulletSpeed += Time.deltaTime;
+            float t = bulletData.bulletSpeed / dis;
+
+            bulletTrans.position = Vector3.LerpUnclamped(bulletTrans.position, playerTrans.position, t);
+        }
+        // 매프레임마다 타겟방향으로 포탄이 방향을바꿉니다
+        //타겟위치 - 포탄위치 = 포탄이 타겟한테서의 방향
+        Vector3 directionV3 = playerTrans.position - bulletTrans.position;
+        Quaternion qua = Quaternion.LookRotation(directionV3);
+        bulletTrans.rotation = Quaternion.Slerp(bulletTrans.rotation, qua, Time.deltaTime * 2f);
+    }
+
 }
