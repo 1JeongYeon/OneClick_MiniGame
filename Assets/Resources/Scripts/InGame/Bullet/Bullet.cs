e431cd3b95a65668a2203a474206846c15de2994
1JeongYeon <102153402+1JeongYeon@users.noreply.github.com>
2023-02-01 17:46:31 +0900

    총알 구현중 총알 정보를 담아야함

--- Assets/Resources/Scripts/InGame/Bullet/Bullet.cs
+++ Assets/Resources/Scripts/InGame/Bullet/Bullet.cs
@@ -20,10 +20,17 @@ public abstract class Bullet : MonoBehaviour
 
     private bool shootAble = false;
     private float delayTime = 0f;
+    private Transform playerTrans;
+    private Transform bulletTrans;
+    private float dis;
 
     private void Start()
     {
         delayTime = bulletData.delayTime;
+
+        bulletData.bullet.transform.rotation = Quaternion.LookRotation(transform.position - playerTrans.position);
+        playerTrans = FindObjectOfType<Player>().transform;
+        dis = Vector3.Distance(bulletTrans.position, playerTrans.position);
     }
     public abstract void InitSetting(TMP_Text info);
 
@@ -34,9 +41,11 @@ public abstract class Bullet : MonoBehaviour
         if (shootAble == true)
         {
             var bullet = Instantiate(bulletData.bullet);
-            bullet.transform.position = muzzle.position;
-
+            bullet.transform.position = muzzle.position; // 시작점
+            bulletTrans = bullet.transform;
             
+            DiffusionMissileMoveOperation(bulletData);
+
             var fireEffect = Instantiate(soundText);
             fireEffect.transform.position = muzzle.position + new Vector3(0, 3f, 0);
             fireEffect.text = bulletData.soundEffect;
@@ -56,5 +65,30 @@ public abstract class Bullet : MonoBehaviour
                 delayTime = 0f;
             }
         }
+    }
+
+    // instantiate되는 총알이 이 정보를 담고 있어야 하는데 그러질 못해서 null이 뜨는중
+    private void DiffusionMissileMoveOperation(BulletData bulletData)
+    {
+        delayTime += Time.deltaTime;
+        if (delayTime < 1f)
+        {
+            // 1초동안 천천히 앞으로 전진
+            bulletData.bulletSpeed = Time.deltaTime;
+            transform.Translate(bulletTrans.forward * bulletData.bulletSpeed, Space.Self);
+        }
+        else
+        {
+            // 1초 이후 타겟방향으로 lerp위치이동
+            bulletData.bulletSpeed += Time.deltaTime;
+            float t = bulletData.bulletSpeed / dis;
+
+            bulletTrans.position = Vector3.LerpUnclamped(bulletTrans.position, playerTrans.position, t);
+        }
+        // 매프레임마다 타겟방향으로 포탄이 방향을바꿉니다
+        //타겟위치 - 포탄위치 = 포탄이 타겟한테서의 방향
+        Vector3 directionV3 = playerTrans.position - bulletTrans.position;
+        Quaternion qua = Quaternion.LookRotation(directionV3);
+        bulletTrans.rotation = Quaternion.Slerp(bulletTrans.rotation, qua, Time.deltaTime * 2f);
     }
 }
