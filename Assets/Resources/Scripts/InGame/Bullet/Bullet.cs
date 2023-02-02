91583851ace90feb30be0ad1ba7174c71eae3404
1JeongYeon <102153402+1JeongYeon@users.noreply.github.com>
2023-02-02 17:37:54 +0900

    유도탄 구현중

--- Assets/Resources/Scripts/InGame/Bullet/Bullet.cs
+++ Assets/Resources/Scripts/InGame/Bullet/Bullet.cs
@@ -28,23 +28,22 @@ public abstract class Bullet : MonoBehaviour
     {
         delayTime = bulletData.delayTime;
 
-        bulletData.bullet.transform.rotation = Quaternion.LookRotation(transform.position - playerTrans.position);
         playerTrans = FindObjectOfType<Player>().transform;
-        dis = Vector3.Distance(bulletTrans.position, playerTrans.position);
     }
     public abstract void InitSetting(TMP_Text info);
 
     public virtual void Shooting(Transform muzzle, TMP_Text soundText)
     {
-        
-
         if (shootAble == true)
         {
             var bullet = Instantiate(bulletData.bullet);
             bullet.transform.position = muzzle.position; // 시작점
+
             bulletTrans = bullet.transform;
-            
-            DiffusionMissileMoveOperation(bulletData);
+            dis = Vector3.Distance(bulletTrans.position, playerTrans.position);
+            bulletData.bullet.transform.rotation = Quaternion.LookRotation(transform.position - playerTrans.position);
+
+            DiffusionMissileMoveOperation(bullet.GetComponent<Bullet>());
 
             var fireEffect = Instantiate(soundText);
             fireEffect.transform.position = muzzle.position + new Vector3(0, 3f, 0);
@@ -61,6 +60,7 @@ public abstract class Bullet : MonoBehaviour
             delayTime += Time.deltaTime;
             if (delayTime >= bulletData.delayTime)
             {
+                DestroyImmediate(soundText);
                 shootAble = true;
                 delayTime = 0f;
             }
@@ -68,20 +68,23 @@ public abstract class Bullet : MonoBehaviour
     }
 
     // instantiate되는 총알이 이 정보를 담고 있어야 하는데 그러질 못해서 null이 뜨는중
-    private void DiffusionMissileMoveOperation(BulletData bulletData)
+    private void DiffusionMissileMoveOperation(Bullet _bullet)
     {
+        Debug.Log(" 유도탄 ");
+
+
         delayTime += Time.deltaTime;
         if (delayTime < 1f)
         {
             // 1초동안 천천히 앞으로 전진
-            bulletData.bulletSpeed = Time.deltaTime;
-            transform.Translate(bulletTrans.forward * bulletData.bulletSpeed, Space.Self);
+            _bullet.bulletData.bulletSpeed = Time.deltaTime;
+            transform.Translate(bulletTrans.forward * _bullet.bulletData.bulletSpeed, Space.Self);
         }
         else
         {
             // 1초 이후 타겟방향으로 lerp위치이동
-            bulletData.bulletSpeed += Time.deltaTime;
-            float t = bulletData.bulletSpeed / dis;
+            _bullet.bulletData.bulletSpeed += Time.deltaTime;
+            float t = _bullet.bulletData.bulletSpeed / dis;
 
             bulletTrans.position = Vector3.LerpUnclamped(bulletTrans.position, playerTrans.position, t);
         }
@@ -89,6 +92,6 @@ public abstract class Bullet : MonoBehaviour
         //타겟위치 - 포탄위치 = 포탄이 타겟한테서의 방향
         Vector3 directionV3 = playerTrans.position - bulletTrans.position;
         Quaternion qua = Quaternion.LookRotation(directionV3);
-        bulletTrans.rotation = Quaternion.Slerp(bulletTrans.rotation, qua, Time.deltaTime * 2f);
+        bulletTrans.rotation = Quaternion.Slerp(bulletTrans.rotation, qua, Time.deltaTime * 2.5f);
     }
 }
