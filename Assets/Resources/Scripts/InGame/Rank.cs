310002acc3de664a3ac732d1d1a0a31a3d7e1cc9
1JeongYeon <102153402+1JeongYeon@users.noreply.github.com>
2023-03-26 20:59:01 +0900

    0326

--- Assets/Resources/Scripts/InGame/Rank.cs
+++ Assets/Resources/Scripts/InGame/Rank.cs
@@ -5,41 +5,34 @@ using UnityEngine;
 public class Rank : MonoBehaviour
 {
     private int[] bestScore = new int[] { };
-    private int[] bestTime = new int[] { };
 
-    public void ScoreSet(int currentScore, int currentTime)
+    public GameObject[] rankFrames;
+
+    public void ScoreSet(int currentScore)
     {
         PlayerPrefs.SetInt("CurrentScore", currentScore);
-        PlayerPrefs.SetInt("CurrentTime", currentTime);
 
-        int score = 0;
-        int time = 0;
+        int tempScore = 0;
 
         for (int i = 0; i < bestScore.Length; i++)
         {
             bestScore[i] = PlayerPrefs.GetInt(i + "BestScore");
-            bestTime[i] = PlayerPrefs.GetInt(i + "BestTime");
 
             while (bestScore[i] < currentScore)
             {
                 // 자리 바꿔줌
-                score = bestScore[i];
-                time = bestTime[i];
+                tempScore = bestScore[i];
                 bestScore[i] = currentScore;
-                bestTime[i] = currentTime;
                 // 랭킹 저장
                 PlayerPrefs.SetInt(i + "BestScore", currentScore);
-                PlayerPrefs.SetInt(i + "BestTime", currentTime);
                 // 다음 반복을 위한 준비
-                currentScore = score;
-                currentTime = time;
+                currentScore = tempScore;
             }
         }
 
         for (int i = 0; i < 10; i++)
         {
             PlayerPrefs.SetInt(i + "BestScore", bestScore[i]);
-            PlayerPrefs.SetInt(i + "BestTime", bestTime[i]);
         }
     }
 }
